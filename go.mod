9fcf24760acf444aaa5c92a9f09f08dcf3c89f00
Ruslan <ruslan@bssx.ru>
2024-11-28 00:04:51 +0200

    Wails activate

--- go.mod
+++ go.mod
@@ -6,7 +6,6 @@ toolchain go1.21.5
 
 require (
 	github.com/wailsapp/wails/v2 v2.7.1
-	golang.org/x/net v0.17.0
 	gorm.io/driver/sqlite v1.5.4
 	gorm.io/gorm v1.25.5
 )
@@ -39,6 +38,7 @@ require (
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
 	golang.org/x/exp v0.0.0-20230522175609-2e198f4a06a1 // indirect
+	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/sys v0.13.0 // indirect
 	golang.org/x/text v0.13.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"embed"
+
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
 
 //go:embed all:frontend/dist
@@ -9,28 +13,30 @@ var assets embed.FS
 
 func main() {
 	// Create an instance of the app structure
-	// app := NewApp()
+	app := NewApp()
 
-	setupDb()
-	loader := &DbEpg{db: db}
-	loader.LoadAndParse()
+	go func() {
+		setupDb()
+		loader := &DbEpg{db: db}
+		loader.LoadAndParse()
+	}()
 
-	// // Create application with options
-	// err := wails.Run(&options.App{
-	// 	Title:  "iptv",
-	// 	Width:  1024,
-	// 	Height: 768,
-	// 	AssetServer: &assetserver.Options{
-	// 		Assets: assets,
-	// 	},
-	// 	BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-	// 	OnStartup:        app.startup,
-	// 	Bind: []interface{}{
-	// 		app,
-	// 	},
-	// })
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "iptv",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
 
-	// if err != nil {
-	// 	println("Error:", err.Error())
-	// }
+	if err != nil {
+		println("Error:", err.Error())
+	}
 }
